d4db42f708dccfa4b39381cf860716643315a604
Daniel Kinsman <danielkinsman@riseup.net>
2014-04-11 12:52:02 +1000

    Auto load game on server in debug mode

---
+++ server/startup.cs
@@ -0,0 +1,51 @@
+/*
+
+Copyright 2014 Daniel Kinsman.
+
+This file is part of Stand Alone Map View.
+
+Stand Alone Map View is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Stand Alone Map View is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Stand Alone Map View.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+
+#if DEBUG
+
+using KSP;
+using UnityEngine;
+
+namespace StandAloneMapView
+{
+	[KSPAddon(KSPAddon.Startup.MainMenu, false)]
+	public class Startup : utils.MonoBehaviourExtended
+	{
+		public Startup()
+		{
+			this.LogPrefix = "samv server";
+		}
+
+		public override void Start()
+		{
+			HighLogic.SaveFolder = "default";
+			var game = GamePersistence.LoadGame("persistent", HighLogic.SaveFolder, true, false);
+
+			if(game != null && game.flightState != null && game.compatible)
+            {
+				FlightDriver.StartAndFocusVessel(game, 8);
+            }
+		}
+	}
+}
+
+#endif
