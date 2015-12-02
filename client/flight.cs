a7f79902052697c40a955f32c92ab240c24a2902
Daniel Kinsman <danielkinsman@riseup.net>
2015-12-02 19:23:26 +1100

    Vessel creation without reloading on client side

--- client/flight.cs
+++ client/flight.cs
@@ -26,6 +26,9 @@ using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 
 namespace StandAloneMapView.client
 {
@@ -36,9 +39,16 @@ namespace StandAloneMapView.client
 
         public const string CONTROL_LOCK = "";
 
+        protected ConfigNode DummyVesselConfigNode;
+        protected string DummyPath;
+
         public Flight()
         {
             this.LogPrefix = "samv client";
+            var dummyPath = Path.Combine(
+                Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+                "dummy.protovessel");
+            this.DummyVesselConfigNode = ConfigNode.Load(dummyPath);
             this.socketWorker = new SocketWorker();
         }
 
@@ -210,9 +220,44 @@ namespace StandAloneMapView.client
                 var vesselsToKill = FlightGlobals.Vessels.Where(v => !serverVessels.ContainsKey(v.id)).ToList();
                 foreach(var v in vesselsToKill)
                 {
-                    LogDebug("Vessel {0} ({1}) does not exist on server, killing it", v.name, v.id);
+                    LogDebug("Vessel {0} ({1}) does not exist on server, killing it", v.vesselName, v.id);
                     v.Die();
                 }
+
+                // Could be faster to keep this around and maintain it rather than reconstructing it
+                // every time.
+                var vesselIds = new HashSet<Guid>(FlightGlobals.Vessels.Select(v => v.id));
+                var vesselsToAdd = serverVessels.Where(p => !vesselIds.Contains(p.Key)).Select(p => p.Value);
+                foreach(var v in vesselsToAdd)
+                    CreateVessel(v);
+            }
+            catch(Exception e)
+            {
+                LogException(e);
+                throw;
+            }
+        }
+
+        public void CreateVessel(comms.VesselList.VesselInfo vesselInfo)
+        {
+            try
+            {
+                Log("creating vessel {0} {1}", vesselInfo.name, vesselInfo.id);
+                var pv = new ProtoVessel(this.DummyVesselConfigNode, HighLogic.CurrentGame);
+
+                pv.vesselID = vesselInfo.id;
+                pv.vesselName = vesselInfo.name;
+                pv.vesselType = (VesselType)vesselInfo.type;
+                pv.altitude = vesselInfo.altitude;
+                pv.landed = vesselInfo.landed;
+                pv.landedAt = vesselInfo.landedAt;
+                pv.latitude = vesselInfo.latitude;
+                pv.longitude = vesselInfo.longitude;
+                pv.situation = (Vessel.Situations)vesselInfo.situation;
+                pv.splashed = vesselInfo.splashed;
+                pv.orbitSnapShot = new OrbitSnapshot(vesselInfo.orbit.GetKspOrbit(FlightGlobals.Bodies));
+
+                pv.Load(HighLogic.CurrentGame.flightState);
             }
             catch(Exception e)
             {
