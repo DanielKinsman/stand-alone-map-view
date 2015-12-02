b683cd2b5fa0775c70f26138eeae8fdc78495dbb
Daniel Kinsman <danielkinsman@riseup.net>
2015-12-02 22:51:35 +1100

    separate vessel list checking so we can use it in both flight and tracking station behaviours later

--- client/flight.cs
+++ client/flight.cs
@@ -26,30 +26,22 @@ using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 using UnityEngine;
-using System.Collections.Generic;
-using System.IO;
-using System.Reflection;
 
 namespace StandAloneMapView.client
 {
     [KSPAddon(KSPAddon.Startup.Flight, false)]
     public class Flight : utils.MonoBehaviourExtended
     {
-        public SocketWorker socketWorker;
-
         public const string CONTROL_LOCK = "";
 
-        protected ConfigNode DummyVesselConfigNode;
-        protected string DummyPath;
+        public SocketWorker socketWorker;
+        public VesselChecker VesselChecker;
 
         public Flight()
         {
             this.LogPrefix = "samv client";
-            var dummyPath = Path.Combine(
-                Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
-                "dummy.protovessel");
-            this.DummyVesselConfigNode = ConfigNode.Load(dummyPath);
             this.socketWorker = new SocketWorker();
+            this.VesselChecker = new VesselChecker(this);
         }
 
         public override void Awake()
@@ -210,60 +202,7 @@ namespace StandAloneMapView.client
 
         public void CheckVessels()
         {
-            try
-            {
-                var vesselList = TcpWorker.Instance.Vessels;
-                if(vesselList == null)
-                    return;
-
-                var serverVessels = vesselList.Vessels;
-                var vesselsToKill = FlightGlobals.Vessels.Where(v => !serverVessels.ContainsKey(v.id)).ToList();
-                foreach(var v in vesselsToKill)
-                {
-                    LogDebug("Vessel {0} ({1}) does not exist on server, killing it", v.vesselName, v.id);
-                    v.Die();
-                }
-
-                // Could be faster to keep this around and maintain it rather than reconstructing it
-                // every time.
-                var vesselIds = new HashSet<Guid>(FlightGlobals.Vessels.Select(v => v.id));
-                var vesselsToAdd = serverVessels.Where(p => !vesselIds.Contains(p.Key)).Select(p => p.Value);
-                foreach(var v in vesselsToAdd)
-                    CreateVessel(v);
-            }
-            catch(Exception e)
-            {
-                LogException(e);
-                throw;
-            }
-        }
-
-        public void CreateVessel(comms.VesselList.VesselInfo vesselInfo)
-        {
-            try
-            {
-                Log("creating vessel {0} {1}", vesselInfo.name, vesselInfo.id);
-                var pv = new ProtoVessel(this.DummyVesselConfigNode, HighLogic.CurrentGame);
-
-                pv.vesselID = vesselInfo.id;
-                pv.vesselName = vesselInfo.name;
-                pv.vesselType = (VesselType)vesselInfo.type;
-                pv.altitude = vesselInfo.altitude;
-                pv.landed = vesselInfo.landed;
-                pv.landedAt = vesselInfo.landedAt;
-                pv.latitude = vesselInfo.latitude;
-                pv.longitude = vesselInfo.longitude;
-                pv.situation = (Vessel.Situations)vesselInfo.situation;
-                pv.splashed = vesselInfo.splashed;
-                pv.orbitSnapShot = new OrbitSnapshot(vesselInfo.orbit.GetKspOrbit(FlightGlobals.Bodies));
-
-                pv.Load(HighLogic.CurrentGame.flightState);
-            }
-            catch(Exception e)
-            {
-                LogException(e);
-                throw;
-            }
+            this.VesselChecker.Check();
         }
 
         public void ForceMapView()
---
+++ client/vesselchecker.cs
@@ -0,0 +1,83 @@
+using System.IO;
+using System.Reflection;
+using System.Linq;
+using System.Collections.Generic;
+using System;
+using StandAloneMapView.utils;
+
+namespace StandAloneMapView.client
+{
+    public class VesselChecker
+    {
+        protected ConfigNode DummyVesselConfigNode;
+        protected string DummyPath;
+        protected MonoBehaviourExtended Logger; // totally cheating
+
+        public VesselChecker(MonoBehaviourExtended logger)
+        {
+            this.Logger = logger;
+            var dummyPath = Path.Combine(
+                Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+                "dummy.protovessel");
+            this.DummyVesselConfigNode = ConfigNode.Load(dummyPath);
+        }
+
+        public void Check()
+        {
+            try
+            {
+                var vesselList = TcpWorker.Instance.Vessels;
+                if(vesselList == null)
+                    return;
+
+                var serverVessels = vesselList.Vessels;
+                var vesselsToKill = FlightGlobals.Vessels.Where(v => !serverVessels.ContainsKey(v.id)).ToList();
+                foreach(var v in vesselsToKill)
+                {
+                    this.Logger.LogDebug("Vessel {0} ({1}) does not exist on server, killing it", v.vesselName, v.id);
+                    v.Die();
+                }
+
+                // Could be faster to keep this around and maintain it rather than reconstructing it
+                // every time.
+                var vesselIds = new HashSet<Guid>(FlightGlobals.Vessels.Select(v => v.id));
+                var vesselsToAdd = serverVessels.Where(p => !vesselIds.Contains(p.Key)).Select(p => p.Value);
+                foreach(var v in vesselsToAdd)
+                    CreateVessel(v);
+            }
+            catch(Exception e)
+            {
+                this.Logger.LogException(e);
+                throw;
+            }
+        }
+
+        public void CreateVessel(comms.VesselList.VesselInfo vesselInfo)
+        {
+            try
+            {
+                this.Logger.Log("creating vessel {0} {1}", vesselInfo.name, vesselInfo.id);
+                var pv = new ProtoVessel(this.DummyVesselConfigNode, HighLogic.CurrentGame);
+
+                pv.vesselID = vesselInfo.id;
+                pv.vesselName = vesselInfo.name;
+                pv.vesselType = (VesselType)vesselInfo.type;
+                pv.altitude = vesselInfo.altitude;
+                pv.landed = vesselInfo.landed;
+                pv.landedAt = vesselInfo.landedAt;
+                pv.latitude = vesselInfo.latitude;
+                pv.longitude = vesselInfo.longitude;
+                pv.situation = (Vessel.Situations)vesselInfo.situation;
+                pv.splashed = vesselInfo.splashed;
+                pv.orbitSnapShot = new OrbitSnapshot(vesselInfo.orbit.GetKspOrbit(FlightGlobals.Bodies));
+
+                pv.Load(HighLogic.CurrentGame.flightState);
+            }
+            catch(Exception e)
+            {
+                this.Logger.LogException(e);
+                throw;
+            }
+        }
+    }
+}
