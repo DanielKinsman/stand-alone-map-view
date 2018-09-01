81f6d0773d7a4f3d2b6c0d140dd80cbecec7ea32
Daniel Kinsman <danielkinsman@riseup.net>
2018-09-02 09:22:43 +1000

    update assemblyinfo

--- server/AssemblyInfo.cs
+++ server/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("stand alone map view")]
-[assembly: AssemblyCopyright("2014-2015 Daniel Kinsman")]
+[assembly: AssemblyCopyright("2014-2018 Daniel Kinsman")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("0.21.*")]
+[assembly: AssemblyVersion("0.23.*")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
