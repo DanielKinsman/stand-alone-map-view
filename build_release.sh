cfd86827092a3e29b323c51077d84cd8b85a6b40
Daniel Kinsman <danielkinsman@riseup.net>
2018-09-02 09:30:07 +1000

    make tar gz release too

--- build_release.sh
+++ build_release.sh
@@ -6,5 +6,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd $SCRIPT_DIR
 
 docker build . --tag kerbalmod_ci:latest
+docker run -v $SCRIPT_DIR:/mnt/kerbalmod kerbalmod_ci:latest make release.tar.gz
 docker run -v $SCRIPT_DIR:/mnt/kerbalmod kerbalmod_ci:latest make release.zip
 chown -R $SUDO_USER:$SUDO_USER *
